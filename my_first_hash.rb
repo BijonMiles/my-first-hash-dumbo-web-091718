ede77ad1aeef393953b6e6a87fd71b376b39c58e
Bijon Miles <bijonmiles01@gmail.com>
2018-09-12 22:35:52 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -41,7 +41,7 @@ def adding
 shipping_manifest = { 
   # add 4 gun powder to the shipping_manifest hash below
   shipping_manifest = {
-    "muskets" => 2,
+
     "gun powder" => 4,
   }
   puts shipping_manifest
